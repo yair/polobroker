f1989a93d34b371f3f1a863b34d7d59f2332fcbe
Yair Mahalalel <yairmlll@gmail.com>
2018-06-05 12:13:21 +0000

    adding fs watcher and triggering mechanism

--- polobroker.js
+++ polobroker.js
@@ -1,8 +1,15 @@
 const Poloniex = require('poloniex-api-node');
 const fs = require('fs');
+var Inotify = require('inotify').Inotify;
+var inotify = new Inotify();
 
 const LOG_STREAMS = false;
 const LOG_ACTIONS = true;
+const VOLATILE_DIR = '/home/yair/w/volatile/';
+const ORDERS_FN = 'orders.json';
+const PAPER_TRADE = true; // Set orders on top of existing ones and don't execute
+
+var acts = { sells: {}, buys: {} };
 
 const basedir = '/home/yair/w/dm_raw/sr/' + `${Math.floor(new Date() / 1000)}/`;
 fs.mkdirSync(basedir);
@@ -10,6 +17,7 @@ const actionsdir = `${basedir}actions/`;
 fs.mkdirSync(actionsdir);
 console.log(`Base folder created at ${basedir}`);
 let markets = {};
+var timer = null;
 
 let poloniex = new Poloniex();
 
@@ -24,7 +32,7 @@ poloniex.returnTicker().then((ticker) => {
     for (market in Object.keys(ticker)) {
         let mname = Object.keys(ticker)[market];
 
-        if (mname != 'BTC_ETH') {     // delme
+        if (mname != 'BTC_USDT') {     // delme
             continue;
         }
 
@@ -63,10 +71,17 @@ poloniex.on('message', (channelName, data, seq) => {
 
 function updateFromTicker (data, seq) {
 
-//    We will want price and volume stats from here, eventually
+//    We will want price and volume stats from here, eventually, but also maybe updating live balances. How is this different from 'newTrade' event?
 }
 
 function updateFromStream (mname, payload, seq) {
+
+    let lives = [];
+    if (Object.keys(acts['sells']).length != 0) {
+        lives = acts['sells'];
+    } else if (Object.keys(acts['buys']).length != 0) {
+        lives = acts['buys'];
+    }
 
     for (item in payload) {
         let item_type = payload[item]['type'];
@@ -84,11 +99,36 @@ function updateFromStream (mname, payload, seq) {
             }
         } else if (item_type == 'orderBookRemove') {
             delete markets[mname][data['type'] == 'ask' ? 'ob_asks' : 'ob_bids'][data['rate']];
+		} else if (item_type == 'newTrade') {
+// {"type":"newTrade","data":{"tradeID":"43691798","type":"buy","rate":"0.07939500","amount":"0.00213805","total":"0.00016975","date":"2018-06-05T10:52:06.000Z"}}
+			// Update price and volume stats, I guess
+            // No! This is also for adjusting remaining amount to trade! TODO
+            if (mname in Object.keys(lives)) {
+                if (data['rate'] in Object.keys(lives[mname]['orders'])) {
+                    lives[mname]['amount_changed'] = 1;
+                }
+                lives[mname]['trades'].push(data);
+            }
         } else {
-            console.log(`Unfamiliar item type: ${item_type}`);
+            console.log(`Unfamiliar item type: ${item_type} payload: ${JSON.stringify(payload)}`);
+        }
+        if (mname in Object.keys(lives)) {
+            lives[mname]['trigger'] = 1;
         }
     }
 
+//    console.log(`lives: ${JSON.stringify(lives)}`);
+    for (i in Object.keys(lives)) {
+//        console.log(`i=${i} Object.keys(lives)[i]=${Object.keys(lives)[i]}`);
+        let live = Object.keys(lives)[i];
+//        let live = lives[Object.keys(lives)[i]];
+//        let live = lives[i];
+//        console.log(`lives[${live}]: ${lives[live]}`);
+        if (lives[live].hasOwnProperty(['trigger']) && lives[live]['trigger'] == 1) {
+            console.log(`Triggering ${live} from stream update`);
+            trigger(live);
+        }
+    }
     // updateOBstats()
     //
     // if ('trading' in Object.keys(markets['mname']))
@@ -106,9 +146,105 @@ function getSortedOB (ob) {
     return Object.keys(ob).sort().map(x => [x, ob[x]]);
 }
 
+function trigger (mname) {
+    console.log(`Triggered: ${mname}`);
+}
+
+function triggerAll () {
+
+    if (Object.keys(acts['sells']).length != 0) {
+        for (act in Object.keys(acts['sells'])) {
+//            trigger(acts['sells'][Object.keys(acts['sells'])[act]]);
+            trigger(Object.keys(acts['sells'])[act]);
+        }
+    } else if (Object.keys(acts['buys']).length != 0) { // Don't start buying until sells are done.
+        for (act in Object.keys(acts['buys'])) {
+//            trigger(acts['buys'][Object.keys(acts['buys'])[act]]);
+            trigger(Object.keys(acts['buys'])[act]);
+        }
+    } else {
+        console.log("triggerAll called with nothing to do. Finalizing.");
+        clearInterval(timer);
+		fs.unlink (VOLATILE_DIR + ORDERS_FN, function (err) { 
+			console.log(`${VOLATILE_DIR + ORDERS_FN} read and removed.`);
+		});
+    }
+}
+
+/*
+function launchAllSells() {
+	let triggered = 0;
+	for (market in Object.keys(markets)) {
+		if (markets[market]['action']['type'] == 'Sell') {
+			triggered++;
+			trigger(markets[market]);
+		}
+	}
+    if (triggered == 0) {
+        launchAllBuys();
+    }
+
+}*/
+
+// {"timeout": 60, "actions": [["Buy", "BTC_USDT", 0, 74.86000318514343]]}
+function processOrders () {
+    if (Object.keys(acts['buys']) != 0 || Object.keys(acts['sells']) != 0) {
+        console.log("ERROR: New batch arrived while older orders are still being processed. Aborting new batch!");
+        return;
+    }
+	let fn = VOLATILE_DIR + ORDERS_FN;
+	fs.readFile (fn, 'utf8', function (err, data) {
+        if (err) {
+            console.log(`Error reading file ${fn}: ${err}`);
+            return;
+        }
+		json = JSON.parse(data);
+		timeout = json['timeout'];
+		actions = json['actions'];
+//		let sells = [];
+//		let buys = [];
+		for (i in actions) {
+            action = actions[i];
+			let act = {
+				start: Date.now(),
+				timeout: timeout,
+				prev_balance: action[2],
+				total_amount: action[3],
+				type: action[0],
+                orders: {},
+                amount_changed: 0,
+                trades: [],
+			};
+            console.log(`action: ${action}`);
+            if (action[0] == 'Sell') {
+                acts['sells'][action[1]] = act;
+            } else if (action[0] == 'Buy') {
+                acts['buys'][action[1]] = act;
+            } else {
+                console.log(`Unknown action: ${action[0]}`);
+            }
+		}
+//		launchAllSells();
+        triggerAll();
+        timer = setInterval (triggerAll, 1000);
+	});
+}
+
 poloniex.on('open', () => {
     console.log(`Poloniex WebSocket connection open. Opening fs listener.`);
-    
+/*	fs.watch('/home/yair/w/volatile/', {}, (eventType, filename) => {
+		console.log(`Filename: ${filename}, eventType: ${eventType}`);
+	});*/
+	inotify.addWatch({
+		path:		VOLATILE_DIR,
+		watch_for:	Inotify.IN_CLOSE,
+		callback:	function (event) {
+			console.log(`${event.name} closed.`);
+			if (event.name == ORDERS_FN) {
+				processOrders();
+			}
+		}
+	});
 });
 
 poloniex.on('close', (reason, details) => {
